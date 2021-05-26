8de8d9d9d763cd15896f1e936ea6f842108b7b39
Chidiebere Enekwe <chidieberehenry31@gmail.com>
2021-05-26 16:42:53 +0100

    initial commit

---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from PyDictionary import PyDictionary
+# Create your views here.
+@csrf_exempt
+def home(request):
+    return render(request, 'home/home.html')
+
+@csrf_exempt
+def word(request):
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    synonyms = dictionary.synonym(search)
+    antonyms = dictionary.antonym(search)
+    translations = dictionary.translate(search, 'french, greek, latin')
+    context = {
+        'meaning':meaning,
+        'synonyms':synonyms,
+        'antonyms':antonyms,
+        'translations': translations,
+    }
+    return render(request, 'home/word.html', context)
